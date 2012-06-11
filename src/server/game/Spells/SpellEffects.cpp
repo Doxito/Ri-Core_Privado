c4989b6c95fdbba5a6fd5ff631af55848b97e96b
Kandera <KanderaDev@gmail.com>
2012-06-11 12:22:15 -0400

    Core/Spells: use raw basepoint value for extra attacks. damage gets fuddled somewhere for this spell. fixes hardened steel berserker doing insane damages!

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5329,7 +5329,7 @@ void Spell::EffectAddExtraAttacks(SpellEffIndex effIndex)
     if (unitTarget->m_extraAttacks)
         return;
 
-    unitTarget->m_extraAttacks = damage;
+    unitTarget->m_extraAttacks = GetSpellInfo()->Effects[effIndex].BasePoints;
 
     ExecuteLogEffectExtraAttacks(effIndex, unitTarget->getVictim(), damage);
 }

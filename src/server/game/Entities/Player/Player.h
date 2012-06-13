84c1665b6feb86c8a85272da87fd75a2003f3d0a
kaelima <kaelima@live.se>
2012-06-13 08:21:29 +0200

    Core/DungeonFinder:
    - Corrections to c6c23b7f7ef148f88b64346a44a46f51d2cad212: Properly fixed #6748 after misunderstanding the issue.
    - Display proper error message when teleportating as fatigued

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2008,6 +2008,7 @@ class Player : public Unit, public GridObject<Player>
             StopMirrorTimer(BREATH_TIMER);
             StopMirrorTimer(FIRE_TIMER);
         }
+        bool IsMirrorTimerActive(MirrorTimerType type) { return m_MirrorTimer[type] == getMaxTimer(type); }
 
         void SetMovement(PlayerMovementType pType);
 

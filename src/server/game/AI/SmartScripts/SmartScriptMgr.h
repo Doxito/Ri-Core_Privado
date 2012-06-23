663b40bf9342b66954cd8342275f82f2e9d819c8
Shauren <shauren.trinity@gmail.com>
2012-06-23 12:15:52 +0200

    Core/SmartAI: Fixed SMART_ACTION_SET_RANGED_MOVEMENT parameters

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -355,11 +355,11 @@ struct SmartEvent
             uint32 eventId;
         } eventInform;
 
-        struct 
+        struct
         {
             uint32 eventId;
         } doAction;
-        
+
         struct
         {
             uint32 param1;
@@ -856,9 +856,9 @@ struct SmartAction
 
         struct
         {
-            bool withDelayed;
+            uint32 withDelayed;
             uint32 spell_id;
-            bool withInstant;
+            uint32 withInstant;
         } interruptSpellCasting;
 
         struct
@@ -905,9 +905,12 @@ struct SmartAction
 
         struct
         {
-            float distance;
-            float angle;
+            uint32 distance;
+            uint32 angle;
         } setRangedMovement;
+
+        //! Note for any new future actions
+        //! All parameters must have type uint32
 
         struct
         {

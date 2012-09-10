d8bbb7dfeda07db2ceccdebc75ea1dad9da1c990
unknown <User_1@User_1-PC.(none)>
2012-09-10 06:07:40 +0200

    Sql comando silenciar

---
+++ sql/RI/world/10-09-2012 Comando Silenciar.sql
@@ -0,0 +1,4 @@
+INSERT INTO command(NAME,SECURITY, HELP) VALUES ("silenciar", '3', 'Sintaxis: .mute [$NonmbreJugador] $TiempoEnMinutos
+
+Deshabilita los mensages de chat del personaje de la cuenta de un $NonmbreJugador (o el actualmente seleccionado) durante $TiempoEnMinutos.
+El jugador pararecerá como offline.')
